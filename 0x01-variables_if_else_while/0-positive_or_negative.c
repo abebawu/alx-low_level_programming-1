4aa82a4cc083294396b467954ae94b1fb92c2cdc
Dakkaa <86023061+Dakkaa@users.noreply.github.com>
2022-06-03 23:45:04 +0300

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,10 +2,10 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - Determines if a number is positive, negative or zero.
- *
- * Return: Always 0 (Success)
- */
+* main - Determines if a number is positive, negative or zero.
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 int n;
@@ -13,15 +13,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf(%d is positiven, n);
+printf("%d is positive\n", n);
 }
 else if (n == 0)
 {
-printf(%d is zeron, n);
+printf("%d is zero\n", n);
 }
 else
 {
-printf(%d is negativen, n);
+printf("%d is negative\n", n);
 }
 return (0);
 }
