d6a26f540331775d258d9d7fea53ddf4bc23a9fc
Dakkaa <86023061+Dakkaa@users.noreply.github.com>
2022-06-07 16:06:06 +0300

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,27 +1,14 @@
-#include holberton.h
-
+#include "main.h
 /**
-
 * add - Adds two integers and returns
-
 * the result.
-
 *
-
 * @a: number one.
-
 * @b: number two.
-
 *
-
 * Return: Add of number one and number two.
-
 */
-
 int add(int a, int b)
-
 {
-
 return (a + b);
-
 }
