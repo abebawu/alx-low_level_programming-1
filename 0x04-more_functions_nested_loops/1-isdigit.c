33054814041a9d2985f6e13751beead958066e8e
Daka Sode <dakadegu11@gmail.com>
2022-06-09 15:40:02 -0700

    C function nasted loop

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+
+
+/**
+
+ * _isdigit - checks if parameter is a number between 0 to 9.
+
+ * @c: input number.
+
+ * Return: 1 if is a number (0 to 9), 0 in other case.
+
+ */
+
+int _isdigit(int c)
+
+{
+
+0-isupper.c if (c >= 48 && c <= 57)
+
+0-isupper.c {
+
+0-isupper.c 0-isupper.c return (1);
+
+0-isupper.c }
+
+0-isupper.c else
+
+0-isupper.c {
+
+0-isupper.c 0-isupper.c return (0);
+
+0-isupper.c }
+
+}
