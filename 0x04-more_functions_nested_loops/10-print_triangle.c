8df4b5cd9194d564e0880a92154ff4cbd66b9b31
Dakkaa <86023061+Dakkaa@users.noreply.github.com>
2022-06-10 02:21:16 +0300

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,15 +7,15 @@ void print_triangle(int size)
 {
 int h;
 int a;
-if (size <= 0)
+if (size<=0)
 _putchar('\n');
-for (a = 0; a < size; a++)
+for (a=0;a<size;a++)
 {
-for (h = size; h - 1 > a; h--)
+for (h=size;h-1>a;h--)
 {
 _putchar(' ');
 }
-for (h = 0; h - 1 < a; h++)
+for (h=0;h- 1<a;h++)
 {
 _putchar('#');
 }
