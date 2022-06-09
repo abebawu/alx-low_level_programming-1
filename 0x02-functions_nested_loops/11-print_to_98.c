82c91f25b5923d71abe66af005e58e0ccb0e1a28
Dakkaa <86023061+Dakkaa@users.noreply.github.com>
2022-06-10 02:36:55 +0300

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,41 +9,19 @@
 */
 void print_to_98(int n)
 {
-while (n != 98)
-{
-if (n >= 98)
-{
-if (n >= 100)
-_putchar((n / 100) + '0');
-_putchar(((n / 10) % 10) + '0');
-_putchar((n % 10) + '0');
-n--;
-}
-else if (n < 0)
-{
-_putchar('-');
-n = (n * -1);
-if (n >= 100)
-_putchar((n / 100) + '0');
-if (n >= 10)
-_putchar(((n / 10) % 10) + '0');
-_putchar((n % 10) + '0');
-n--;
-n = (n * -1);
-}
-else if (n >= 0)
-{
-if (n >= 10)
-{
-_putchar((n / 10) + '0');
-}
-_putchar((n % 10) + '0');
-n++;
-}
-_putchar(',');
-_putchar(' ');
-}
-_putchar((n / 10) + '0');
-_putchar((n % 10) + '0');
-_putchar('\n');
+	if (n > 98)
+	{
+		for (; n > 98; n--)
+		{
+			printf("%d, ", n);
+		}
+	}
+	else if (n < 98)
+	{
+		for (; n < 98; n++)
+		{
+			printf("%d, ", n);
+		}
+	}
+	printf("%d\n", n);
 }
