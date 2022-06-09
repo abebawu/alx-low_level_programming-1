3eea792eb4ef31f31fbf6d67fa57a1a3d99680cd
Dakkaa <86023061+Dakkaa@users.noreply.github.com>
2022-06-10 01:47:04 +0300

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,43 +1,20 @@
-#include "holberton.h"
-
-
-
+#include "main.h"
 /**
-
- * print_triangle - prints a triangle.
-
- * @size: numbers of lines.
-
- * Return: no return.
-
- */
-
+* print_triangle - prints a triangle.
+* @size: numbers of lines.
+* Return: no return.
+*/
 void print_triangle(int size)
-
 {
-
-int i, j;
-
-
-
+  int i, j;
 for (i = 0; i < size; i++)
-
 {
-
 for (j = 1; j < (size - i); j++)
-
-_putchar( );
-
+_putchar(' ');
 for (j--; j < size; j++)
-
 _putchar(35);
-
 if (i < (size - 1))
-
-_putchar(n);
-
+_putchar('\n');
 }
-
-_putchar(n);
-
+_putchar('\n');
 }
