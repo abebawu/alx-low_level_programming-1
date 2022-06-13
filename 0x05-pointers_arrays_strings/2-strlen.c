d05d39bf0b799aada1b86c9624a6b039c81c7d37
Dakkaa <86023061+Dakkaa@users.noreply.github.com>
2022-06-13 23:31:37 +0300

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,27 +1,14 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
-
  * _strlen - returns the length of a string.
-
  * @s: input string.
-
  * Return: length of a string.
-
  */
-
 int _strlen(char *s)
-
 {
+	int count = 0;
 
-int count = 0;
-
-
-
-while (*(s + count) != 0)
-
-count++;
-
-return (count);
-
+	while (*(s + count) != '\0')
+		count++;
+	return (count);
 }
